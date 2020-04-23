c923499a34028c31a261228f039c072fd31d5761
IlyaToritsyn <toritsyn41@gmail.com>
2020-04-23 11:51:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        enum  Operation { Add, Mul, Div=100, Sub = 50};
+
+
+        static void Main(string[] args)
+        {
+
+            Operation op;
+            op = Operation.Add;
+
+            switch (op)
+            {
+                case Operation.Add: break;
+                case Operation.Div: break;
+                case Operation.Mul: break;
+                case Operation.Sub: break;
+
+            }
+            op = Operation.Add + 2;
+            Console.WriteLine(op);
+
+        }
+    }
+}
